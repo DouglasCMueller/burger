bc5390a26998d8f87296026cc8730fb293da0698
Douglas Mueller <douglasmueller6@gmail.com>
2020-02-05 10:28:55 -0500

    changed config

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+let connection;
+if (process.env.CLEARDB_DATABASE_URL='mysql://b522b39542d745:d39fc5c0@us-cdbr-iron-east-04.cleardb.net/heroku_6522e6b8ebb96d0?reconnect=true') {
+  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL='mysql://b522b39542d745:d39fc5c0@us-cdbr-iron-east-04.cleardb.net/heroku_6522e6b8ebb96d0?reconnect=true');
+         
+  }
+  else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Identity&1",
   database: "burgers_db"
-});
+  })
+};
+
+
+
+
+
 
 // Make connection.
 connection.connect(function (err) {
