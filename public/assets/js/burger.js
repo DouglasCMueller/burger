953d0e56c227b05358cac0519e3662fac6ddfb2f
Douglas Mueller <douglasmueller6@gmail.com>
2020-02-06 15:07:07 -0500

    deleted comments

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,26 +1,5 @@
 $(function() {
-    // $(".change-devoured").on("click", function(event) {
-    //   var id = $(this).data("id");
-    //   var newDevoured = $(this).data("newDevoured");
-  
-    //   var newDevouredState = {
-    //     devoured: newDevoured
-    //   };
-  
-    //   // Send the PUT request.
-    //   $.ajax("/api/burgers/" + id, {
-    //     type: "PUT",
-    //     data: newDevouredState
-    //   }).then(
-    //     function() {
-    //       console.log("changed devoured to", newDevoured);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
-  
-    $(".create-form").on("submit", function(event) {
+     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
