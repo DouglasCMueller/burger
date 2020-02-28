c7cf53d07a9a3c73167cb667e4059348f8b06ddd
Douglas Mueller <douglasmueller6@gmail.com>
2020-02-28 11:20:52 -0500

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 let connection;
-if (process.env.CLEARDB_DATABASE_URL) {
-  console.log('CONNECTED TO EXTERNAL DB')
-  connection = mysql.createConnection('mysql://b522b39542d745:d39fc5c0@us-cdbr-iron-east-04.cleardb.net/heroku_6522e6b8ebb96d0?reconnect=true');
+if (process.env.JAWSDB_URL) {
+    console.log('CONNECTED TO EXTERNAL DB')
+    connection = mysql.createConnection('mysql://o4uaj4x263ylyj47:k4sj04zmi6ijm5iy@kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d20uokybwodsoumj');
 } else {
-  console.log('CONNECTED TO LOCAL DB')
-  connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Identity&1",
-  database: "burgers_db"
-  });
+    console.log('CONNECTED TO LOCAL DB')
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Identity&1",
+        database: "burgers_db"
+    });
 }
 
 
 // Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
